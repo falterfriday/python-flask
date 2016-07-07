3cbaa504aa51e90ac438716357ef077e80eb3756
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-07 15:20:52 -0400

    commented hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
-from flask import Flask
-app =  Flask(__name__)
+from flask import Flask #imports flask
+app =  Flask(__name__) #tells flask to run the file
 
-@app.route('/')
+@app.route('/') 	#@ symbol = 'decorator' 
+					#that attaches the following function to the '/'
+					#so when we run localhost:5000/ 
+					#the following function will run
 def hello_world():
 	return "Hello, World!"
 
-app.run(debug=True)
+app.run(debug=True)	#runs in debug mode to display errors
