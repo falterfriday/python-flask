e9376b08ea9916d39049f99d6ce142568307e58d
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-07 17:26:02 -0400

    so much... oh so much

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
-from flask import Flask #imports flask
-app =  Flask(__name__) #tells flask to run the file
+from flask import Flask, render_template	##imports flask and grabs template dir
+app =  Flask(__name__) ##tells flask to run the file
 
-@app.route('/') 	#@ symbol = 'decorator' 
-					#that attaches the following function to the '/'
-					#so when we run localhost:5000/ 
-					#the following function will run
+@app.route('/') 	##@ symbol = 'decorator' 
+					##that attaches the following function to the '/'
+					##so when we run localhost:5000/ 
+					##the following function will run
 def hello_world():
-	return "Hello, World!"
+	return render_template('index.html', name="Patrick")
 
-app.run(debug=True)	#runs in debug mode to display errors
+@app.route('/success')
+def success():
+	return render_template('success.html')
+
+app.run(debug=True)	##runs in debug mode to display errors
